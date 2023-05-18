9a2c8b0592aa4b58579d8f7b0472488a1e0ed35d
Dioneer <Gulkina_em@mail.ru>
2023-05-18 14:12:07 +0300

    troubles with parse

--- src/components/language/language.js
+++ src/components/language/language.js
@@ -1,5 +1,5 @@
 import { useRef, Suspense } from "react";
-import { useTranslation, Trans } from "react-i18next";
+import { useTranslation } from "react-i18next";
 import TodoLoader from '../loader/loader.js'
 import './language.css';
 
@@ -7,27 +7,27 @@ function Language() {
 	const languageRef = useRef(null);
 	const { t, i18n } = useTranslation();
 
-	const changeLanguage = (lang) => {
-		i18n.changeLanguage(lang)
-		// const child = languageRef.current.children;
-		// for (let item of child) {
-		// 	item.classList.remove('active');
-		// }
-		// e.target.closest('li').classList.add('active');
+	const changeLanguage = (language, e) => {
+		i18n.changeLanguage(language)
+		const child = languageRef.current.children;
+		for (let item of child) {
+			item.classList.remove('active');
+		}
+		e.target.closest('li').classList.add('active');
 	}
 
 	return (
 		<Suspense fallback={<TodoLoader></TodoLoader>}>
 			<ul className="changeLanguage" ref={languageRef}>
 				<li className='active'
-					onClick={(e) => { changeLanguage('en') }}>
+					onClick={(e) => { changeLanguage("ru", e) }}>
 					<span>ru</span>
 				</li>
 				<li className=''
-					onClick={(e) => { changeLanguage('ru') }}>
+					onClick={(e) => { changeLanguage("en", e) }}>
 					<span>en</span>
 				</li>
-				<div className="aside__title title mx-2.5"><Trans i18nKey="opportunity">opportunity</Trans></div>
+				<div className="aside__title title mx-2.5">{t("main.opportunitytr")}</div>
 			</ul>
 		</Suspense >
 	)
--- src/i18n.js
+++ src/i18n.js
@@ -1,19 +1,39 @@
 import i18n from 'i18next';
-import Backend from 'i18next-http-backend';
-import { initReactI18next } from 'react-i18next';
+import HttpApi from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
+import { initReactI18next } from 'react-i18next';
 
-i18n.use(Backend).use(initReactI18next).use(LanguageDetector).init({
-	supportedLngs: ["ru", "en"],
-	fallbackLng: "en",
-	debug: true,
-	detection: {
-		order: ['queryString', 'cookie', 'localStorage'],
-		caches: ['cookie']
-	},
-	interpolation: {
-		escapeValue: false
-	}
-});
+i18n
+	.use(HttpApi)
+	.use(LanguageDetector)
+	.use(initReactI18next)
+	.init({
+		supportedLngs: ['ru', 'en'],
+		fallbackLng: 'ru',
+		defaultLocale: 'en',
+		locales: ['ru', 'en'],
+		debug: true,
+		ns: ['translation'],
+		defaultNS: 'translation',
+		detection: {
+			order: ['cookie', 'localStorage'],
+			caches: ['cookie']
+		},
+		interpolation: {
+			escapeValue: false, // not needed for react as it escapes by default
+		},
+		backend: {
+			crossDomain: true,
+			withCredentials: false,
+			overrideMimeType: false,
+			requestOptions: {
+				// used for fetch, can also be a function (payload) => ({ method: 'GET' })
+				mode: 'cors',
+				credentials: 'same-origin',
+				cache: 'default'
+			},
+			reloadInterval: false
+		},
+	});
 
 export default i18n;
